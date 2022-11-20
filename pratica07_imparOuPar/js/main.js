456923e317e77d9e10d37dc3beef75d87975f375
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-11-20 11:27:50 -0300

    refactor: script finalizado

--- pratica07_imparOuPar/js/main.js
+++ pratica07_imparOuPar/js/main.js
@@ -1,4 +1,4 @@
-const inputNumber = document.querySelector("#num");
+const inputNumber = document.querySelector("#input-number");
 const btnSend = document.querySelector("#send");
 const result = document.querySelector("#result");
 
@@ -7,36 +7,37 @@ const send = function (e) {
 
   const btnRadioImpar = document.querySelector("#impar");
   const btnRadioPar = document.querySelector("#par");
+
   const min = 1;
   const max = 10;
-  let num1 = Number(inputNumber.value);
-  let num2 = Number(Math.floor(Math.random() * (max - min) + min));
-  let soma = num1 + num2;
+  let numberYou = Number(inputNumber.value);
+  let numberPc = Number(Math.floor(Math.random() * (max - min) + min));
+  let soma = numberYou + numberPc;
 
-  if (num1 <= 10) {
+  if (numberYou <= 10) {
     if (btnRadioImpar.checked == true || btnRadioPar.checked == true) {
       if (soma % 2 == 0 && btnRadioPar.checked == true) {
         result.innerText = `
-        A máquina escolheu o número: ${num2}
-        Você escolheu o número: ${num1}
+        A máquina escolheu o número: ${numberPc}
+        Você escolheu o número: ${numberYou}
         Você colorou: ${btnRadioPar.value}
         ---------- Você ganhou! ----------`;
       } else if (soma % 2 == 1 && btnRadioImpar.checked == true) {
         result.innerText = `
-        A máquina escolheu o número: ${num2}
-        Você escolheu o número: ${num1}
+        A máquina escolheu o número: ${numberPc}
+        Você escolheu o número: ${numberYou}
         Você colorou: ${btnRadioImpar.value}
         ---------- Você ganhou! ----------`;
       } else if (soma % 2 == 0 && btnRadioPar.checked == false) {
         result.innerText = `
-        A máquina escolheu o número: ${num2}
-        Você escolheu o número: ${num1}
+        A máquina escolheu o número: ${numberPc}
+        Você escolheu o número: ${numberYou}
         Você colorou: ${btnRadioImpar.value}
         ---------- Você perdeu! ----------`;
       } else if (soma % 2 == 1 && btnRadioImpar.checked == false) {
         result.innerText = `
-        A máquina escolheu o número: ${num2}
-        Você escolheu o número: ${num1}
+        A máquina escolheu o número: ${numberPc}
+        Você escolheu o número: ${numberYou}
         Você colorou: ${btnRadioPar.value}
         ---------- Você perdeu! ----------`;
       }
